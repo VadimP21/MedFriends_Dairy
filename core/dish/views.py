220da03d95061ec298faf1647a2c42d826ccfc83
Vadim_P <vadimp_91@mail.ru>
2026-02-09 20:00:43 +0300

    views.py created

--- core/dish/views.py
+++ core/dish/views.py
@@ -1,17 +1,26 @@
 """
 like core in Base project
 """
-from django.http import HttpResponse, HttpRequest
+import json
 
-from core.dish.shcemas.ProductSchemas import ProductCreate
+from django.http import HttpResponse, HttpRequest, JsonResponse
+
+from core.dish.models import Product
+from core.dish.service import ProductService
+from core.dish.shcemas.product_schemas import ProductCreate, ProductResponse
 
 
 def create_product(
-    request: HttpRequest,
-    payload: ProductCreate,
-    *,
-    product_service: ProductService,
+        request: HttpRequest,
+        product_service: ProductService,
 ):
-    ...
+    body_data = json.loads(request.body.decode('utf-8'))
+    validated_product = ProductCreate(**body_data)
+    # validated_product = ProductCreate(request.body.decode())
 
 
+    response_data = product_service.create_product(payload=validated_product)
+
+    response_dto = ProductResponse.form_orm(response_data)
+
+    return JsonResponse(response_dto.dict, status=201)

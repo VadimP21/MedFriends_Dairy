55a60817bee28473c3608408b0adff780cac4ec8
Vadim_P <vadimp_91@mail.ru>
2026-02-12 20:33:52 +0300

    utils.validate_json_body add

--- core/dish/utils.py
+++ core/dish/utils.py
@@ -17,7 +17,7 @@ def validate_json_request(func):
         try:
             body_unicode = request.body.decode("utf-8")
             print(f"Body: {request.body.decode()}")
-            data = json.loads(request.body.decode().strip())
+            data = json.loads(request.body.decode("utf-8").strip())
         except json.JSONDecodeError as e:
             return JsonResponse(
                 {
@@ -29,6 +29,30 @@ def validate_json_request(func):
             )
 
         request.json_data = data
+        return func(request, *args, **kwargs)
+
+    return wrapper
+
+
+import json
+from functools import wraps
+from django.http import HttpRequest, JsonResponse
+
+
+def validate_json_body(func):
+    """Парсит JSON и сохраняет в request.json_data для методов с телом запроса"""
+
+    @wraps(func)
+    def wrapper(request: HttpRequest, *args, **kwargs):
+        if request.method in ["POST", "PUT", "PATCH"]:
+            if not request.body:
+                return JsonResponse({"error": "Request body is empty"}, status=400)
+            try:
+                request.json_data = json.loads(request.body.decode("utf-8").strip())
+            except json.JSONDecodeError as e:
+                return JsonResponse(
+                    {"error": "Invalid JSON", "details": str(e)}, status=400
+                )
         return func(request, *args, **kwargs)
 
     return wrapper

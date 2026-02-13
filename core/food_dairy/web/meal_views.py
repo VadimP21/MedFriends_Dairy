c3a8b26b58f3a729518e30e4dfb315761b971f1f
Vadim_P <vadimp_91@mail.ru>
2026-02-13 16:10:43 +0300

    get_by_photo view add

--- core/food_dairy/web/meal_views.py
+++ core/food_dairy/web/meal_views.py
@@ -2,10 +2,12 @@
 like web.py in Base project
 """
 
+import os
 from datetime import datetime
 from typing import List
 
 from django.contrib.auth.models import User
+from django.core.files.storage import default_storage
 from django.db import transaction
 from django.http import HttpRequest, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
@@ -17,7 +19,8 @@ from core.food_dairy.schemas.dish_schemas import DishCreate, DishResponse, DishU
 from core.food_dairy.schemas.meal_schemas import (
     MealCreate,
     MealResponse,
-    AllMealsResponse, MealUpdate,
+    AllMealsResponse,
+    MealUpdate,
 )
 from core.food_dairy.utils import (
     validate_json_request,
@@ -28,8 +31,33 @@ from core.food_dairy.utils import (
 
 @csrf_exempt
 @validate_json_body
-@require_http_methods(["GET"])
-def get_by_photo(): ...
+@require_http_methods(["POST"])
+def get_by_photo(request: HttpRequest):
+    # user = request.user.id
+    # ---------- DEVELOPING -------------
+    user = User.objects.get(id=1)
+    # ---------- DEVELOPING -------------
+    photo_file = request.FILES.get("photo")
+    if not photo_file:
+        return JsonResponse({"error": "Файл 'photo' не найден"}, status=400)
+    file_name = default_storage.save(f"temp_photos/{user}{photo_file.name}", photo_file)
+    file_path = default_storage.path(file_name)
+    meal_model = MealService.meal_photo_handler(file_path)
+
+    meal_response = MealResponse.model_validate(meal_model)
+    response = AllMealsResponse(name="meal", components=[meal_response])
+
+    if os.path.exists(file_path):
+        os.remove(file_path)
+
+    return JsonResponse(
+        {
+            "success": True,
+            "data": response.model_dump(),
+            "message": "Meal created successfully",
+        },
+        status=201,
+    )
 
 
 @csrf_exempt
@@ -129,7 +157,7 @@ def food_diary_view(request: HttpRequest):
         )
 
     elif request.method == "DELETE":
-        meal_id = request.GET.get('mealID')
+        meal_id = request.GET.get("mealID")
         MealService.delete_meal(meal_id=int(meal_id))
 
         return JsonResponse({"status": "Meal deleted"})
